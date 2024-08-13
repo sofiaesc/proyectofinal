c713f035b109e61d3e49b78ce1806aced8038ccf
NPighin <npighin00@gmail.com>
2024-08-13 20:19:40 -0300

    determinar kernel y radio optimo y graficar lineas de circulos externos

--- main.py
+++ main.py
@@ -22,13 +22,31 @@ plot(image, '2_only circles')
 #image       = gamma_correction(image)
 #plot(image, '4_gamma_corrected')
 
-
+more_accurate = float('inf')
+opt_kernel = None
+opt_radius = None
 gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-for radius in range(80,100):
-    for kernel in range(3,6):
-        wells = circle_detection(gray_image, radius)
-        print(len(wells))
-        plot_w_wells(image, wells, str(len(wells))+'finded_'+str(radius)+'-'+str(kernel))
+for radius in range(87, 93):
+    for kernel in range(3, 6, 2):
+        wells = circle_detection(gray_image, radius, kernel)
+        
+        image_draw = image.copy()
+        rightmost_wells                 = find_rightmost_circles(wells)
+        result_image_right, dist_right  = draw_best_fit_line(image_draw, rightmost_wells)
+        leftmost_wells                  = find_leftmost_circles(wells)
+        result_image_left, dist_left    = draw_best_fit_line(result_image_right, leftmost_wells)
+        topmost_wells                   = find_topmost_circles(wells)
+        result_image_up, dist_up        = draw_best_fit_line(result_image_left, topmost_wells)
+        bottommost_wells                = find_bottommost_circles(wells)
+        result_image_down, dist_down    = draw_best_fit_line(result_image_up, bottommost_wells)
+        
+        total_dist = dist_left+dist_right+dist_up+dist_down
+        if total_dist<more_accurate:
+            more_accurate = total_dist
+            opt_kernel = kernel
+            opt_radius = radius
+        print(radius, kernel, "Distancia total: ", total_dist)
+        plot_w_wells(result_image_down, wells, str(len(wells)) + ' finded_' + str(radius) + '-' + str(kernel))
 
 
 
--- utils/utils_detection.py
+++ utils/utils_detection.py
@@ -1,20 +1,106 @@
 import cv2 as cv
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 #--------------------------------------------------------------------------#
 #--------------------------------------------------------------------------#
 #--------------------------------------------------------------------------#
 
-def circle_detection(image, radius, g_kernel=5):
-    blurred_image = cv.GaussianBlur(image, (g_kernel, g_kernel), 0) #La imagen tiene que ser gris
-    circles = cv.HoughCircles(blurred_image, cv.HOUGH_GRADIENT, dp=2.5, minDist=radius*2+5, param1=23, param2=11, minRadius=radius, maxRadius=radius)
+def circle_detection(image, radius, g_kernel):
+    height, width = image.shape[:2]
+    blurred_image = cv.GaussianBlur(image, (g_kernel, g_kernel), 0)
+    circles = cv.HoughCircles(blurred_image, cv.HOUGH_GRADIENT, dp=2.5, minDist=radius*2+5, param1=23, param2=0.9, minRadius=radius, maxRadius=radius)
     wells = []
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
         for circle in circles:
             x, y, r = circle
-            wells.append((x, y, r))
+            if r <= x <= (width - r) and r <= y <= (height - r):
+                wells.append((x, y, r))
+    
     return wells
+
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+
+def find_rightmost_circles(wells, n=8):
+    wells_sorted = sorted(wells, key=lambda c: c[0], reverse=True)
+    return wells_sorted[:n]
+
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+
+def find_leftmost_circles(wells, n=8):
+    wells_sorted = sorted(wells, key=lambda c: c[0])
+    return wells_sorted[:n]
+
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+
+def find_topmost_circles(wells, n=12):
+    wells_sorted = sorted(wells, key=lambda c: c[1])
+    return wells_sorted[:n]
+
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+
+def find_bottommost_circles(wells, n=12):
+    wells_sorted = sorted(wells, key=lambda c: c[1], reverse=True)
+    return wells_sorted[:n]
+
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+
+def draw_best_fit_line(line_image, circles):
+    # Extraer coordenadas de los centros de los círculos
+    points = np.array([circle[:2] for circle in circles], dtype=np.float32)
+    
+    # Ajuste de línea usando la función de ajuste por mínimos cuadrados
+    if len(points) < 2:
+        return line_image
+    
+    [vx, vy, x, y] = cv.fitLine(points, cv.DIST_L2, 0, 0.01, 0.01)
+    slope = vy / vx
+    intercept = y - slope * x
+    
+    # Determinar los puntos de la línea en la imagen
+    h, w = line_image.shape[:2]
+    pt1 = (0, int(intercept))
+    pt2 = (w, int(slope * w + intercept))
+    
+    # Dibujar la línea en la imagen
+    cv.line(line_image, pt1, pt2, (0, 255, 0), 2)
+    
+    # Dibujar líneas de distancia desde los círculos a la línea
+    total_distance = 0
+    for (x, y, r) in circles:
+        x = int(x)
+        y = int(y)
+        cv.circle(line_image, (x, y), r, (0, 0, 255), 2)
+        
+        # Coefficients of the line equation (Ax + By + C = 0)
+        A = slope
+        B = -1
+        C = intercept
+        
+        # Calcular la distancia desde el círculo a la línea
+        distance = abs(A * x + B * y + C) / np.sqrt(A**2 + B**2)
+        total_distance+=distance
+        # Punto en la línea más cercano al círculo
+        if A != 0:  # Avoid division by zero
+            x_line = (B * (B * x - A * y) - A * C) / (A**2 + B**2)
+            y_line = (A * (-B * x + A * y) - B * C) / (A**2 + B**2)
+            point_on_line = (int(x_line), int(y_line))
+            cv.line(line_image, (x, y), point_on_line, (255, 0, 0), 2)
+    
+    return line_image, total_distance
+
+
 
 #--------------------------------------------------------------------------#
 #--------------------------------------------------------------------------#
@@ -52,7 +138,3 @@ def completar_wells(radio, lista_wells, tolerancia, ancho, alto):
                 circulos_incorrectos.append((int(centro_x), int(centro_y), int(radio)))
 
     return circulos_teoricos, circulos_correctos, circulos_incorrectos
-
-#--------------------------------------------------------------------------#
-#--------------------------------------------------------------------------#
-#--------------------------------------------------------------------------#
