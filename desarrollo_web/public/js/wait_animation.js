2e452ba3e2df2f86cf83cfac20e6a9e5d886c983
NPighin <npighin00@gmail.com>
2024-10-22 23:44:14 -0300

    animacion de cargar mientras llama a python

---
+++ desarrollo_web/public/js/wait_animation.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector('form');  // Seleccionar el formulario
+    const loadingOverlay = document.getElementById('loadingOverlay');
+
+    // Al enviar el formulario
+    form.addEventListener('submit', function(event) {
+        const pocillos_hab = document.querySelector('input[name="test[pocillos_hab]"]');
+        // Llamar a la función para verificar la selección de pocillos
+        const shouldPreventSubmit = window.checkPocillosSelection(pocillos_hab.value); // Obtén el valor del estado
+
+        if (shouldPreventSubmit) {
+            console.log("previno");
+            event.preventDefault(); // Prevenir el envío del formulario si no se seleccionó ningún pocillo
+        } else {
+            console.log("no previno");
+            // Mostrar la animación de carga
+            loadingOverlay.style.display = 'flex';
+
+            // Deshabilitar la interacción con la pantalla
+            form.querySelectorAll('button').forEach(function(button) {
+                button.disabled = true;
+            });
+        }
+    });
+});
